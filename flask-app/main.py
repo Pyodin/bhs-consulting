aa9967641ecd8c67b3b30d75bfd20059de1d1596
pbourhis <pbourhis@example.com>
2023-03-17 00:30:16 +0100

    mvp flask application

---
+++ flask-app/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return "Hello world"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
