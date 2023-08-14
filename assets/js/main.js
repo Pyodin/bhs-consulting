e685a477f8c0ad0a73fc594eed9e7c89e3192e06
pbourhis <pbourhis@example.com>
2023-08-14 13:15:18 +0200

    update 14_08

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,3 @@
-const presentationLink = document.getElementById('presentationLink');
-const servicesLink = document.getElementById('servicesLink');
-const skillsLink = document.getElementById('skillsLink');
-const goToExperiencesLink = document.getElementById('goToExperiencesLink');
-const goToprojectsLink = document.getElementById('goToprojectsLink');
-
-function onResize() {
-  // Hide the "Go to buttons" container when the window width is below 1660 pixels
-  if (window.innerWidth < 1750) {
-    presentationLink.style.display = 'none';
-    servicesLink.style.display = 'none';
-    skillsLink.style.display = 'none';
-    goToExperiencesLink.style.display = 'none';
-    goToprojectsLink.style.display = 'none';
-
-  } else {
-    // set visible
-    presentationLink.style.display = 'block';
-    servicesLink.style.display = 'block';
-    skillsLink.style.display = 'block';
-    goToExperiencesLink.style.display = 'block';
-    goToprojectsLink.style.display = 'block';
-  }
-}
-
-// trigger onResize on each resize event
-window.addEventListener('resize', onResize);
-
 const myNav = document.getElementById('navbar');
 window.addEventListener('scroll', function () {
   if (window.scrollY > 100) {
@@ -38,16 +10,21 @@ window.addEventListener('scroll', function () {
   }
 });
 
-
+// hero omage animation
 // const heroImg = document.getElementById('heroImg');
-// // periodically zoom in and out 
-// setInterval(function() {
-//   heroImg.style.transform = "scale(1.05)";
-//   setTimeout(function() {
-//     heroImg.style.transform = "scale(1)";
-//   }, 1000);
-// }, 2000);
+// setInterval(
+//   function() {
+//     heroImg.animate(
+//       {scale: 1.5}, 
+//       5000);
+//     heroImg.animate(
+//       {scale: 1},
+//       5000);
+//   },
+//   5000
+// );
 
+// typed.js for the hero text
 const typed = document.getElementById('typed')
 let typed_strings = typed.getAttribute('data-typed-items')
 typed_strings = typed_strings.split(',')
